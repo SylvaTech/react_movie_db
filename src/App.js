fe6d2779640cf04f6a54e84e380efefe394f5724
sylvaTech <enochsylvanus@gmail.com>
2023-08-01 18:16:33 +0100

    Added React Router and commenced Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+// Router
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Components
+import { GlobalStyle } from './GlobalStyle';
 import Header from './components/Header';
 import Home from './components/Home';
-
-
-import { GlobalStyle } from './GlobalStyle';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/:movieId' element = { <Movie /> } />
+        <Route path='/*' element = {<NotFound />}/>
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
